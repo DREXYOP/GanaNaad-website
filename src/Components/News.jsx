1902a21950455524844570e8a0f2bf6bb41eaede
DREXYOP <91460817+DREXYOP@users.noreply.github.com>
2023-10-10 23:58:37 +0000

    news page build

--- src/Components/News.jsx
+++ src/Components/News.jsx
@@ -1,34 +1,69 @@
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
 import ListGroup from 'react-bootstrap/ListGroup';
+import Carousel from 'react-bootstrap/Carousel';
 import Navbarhome from './Navbarhome';
-import { useEffect , useState } from 'react';
+import { useEffect, useState } from 'react';
 import Axios from "axios";
 import config from "../config.json"
+import Footer from "./Footer"
 
 
 
 function Newscard() {
-    //eslint-disable-next-line
-    const [latestupload,setLatestupload] = useState([])
-    // console.log(data)
-    useEffect(() => {
-        const api = Axios.create({
-            baseURL: config._baseApiUrl
-        })
-        api.get("v1/news/get/latest")
-          .then(res => {
-            console.log(res)
-            setLatestupload(res.data)
-          })
-          .catch(err => {
-            console.log(err.message)
-          })
-      }, []);
-    return (
-        <>
-        <Navbarhome/>
-       
+  //eslint-disable-next-line
+  const [latestupload, setLatestupload] = useState({});
+  const [todays, setTodays] = useState([]);
+  const [thisweek, setThisweek] = useState([]);
+  const [all, setAll] = useState([]);
+  const [error, setError] = useState("")
+
+
+  useEffect(() => {
+    const api = Axios.create({
+      baseURL: config._baseApiUrl,
+      headers: { 'Authorization': `Bearer ${config.apikey}` }
+    });
+    api.get("/v1/news/get/latest")
+      .then(res => {
+        setLatestupload(res.data);
+
+      })
+      .catch(err => {
+        console.log(err.message)
+      })
+    api.get("/v1/news/get/today")
+      .then(res => {
+        setTodays(res.data)
+
+      })
+      .catch(err => {
+        console.log(err.message)
+      })
+    api.get("/v1/news/get/thisweek")
+      .then(res => {
+        setThisweek(res.data);
+
+      })
+      .catch(err => {
+        console.log(err.message)
+      })
+    api.get("/v1/news/get")
+      .then(res => {
+        setAll(res.data);
+
+      })
+      .catch(err => {
+        console.log(err.message)
+      })
+
+  }, []);
+
+  return (
+    <>
+      <div className='news_main_div'>
+        <Navbarhome />
+
         {/* <div className="newscard">
            
             <Card style={{ width: '18rem' }}>
@@ -50,20 +85,86 @@ function Newscard() {
                 </Card.Body>
             </Card>
     </div> */}
-    <div className='latest_div'>
-    <Card>
-      <Card.Header as="h5">Uploaded today.</Card.Header>
-      <Card.Body>
-        <Card.Title>Special title treatment</Card.Title>
-        <Card.Text>
-          With supporting text below as a natural lead-in to additional content.
-        </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
-      </Card.Body>
-    </Card>
-    </div>
-        </>
-        );
+        <div className='latest_div'>
+          <Card>
+            <Card.Header as="h5">Latest . . . </Card.Header>
+            <Card.Body>
+              <Card.Title>{latestupload.title}</Card.Title>
+              <Card.Text>
+                {latestupload.description}
+              </Card.Text>
+              <Button variant="primary">View article</Button>
+            </Card.Body>
+          </Card>
+        </div>
+        <br></br>
+        <hr></hr>
+        <h3>Today's Uploads</h3>
+        <hr></hr>
+        <div className='all_div'>
+            {all.map(n => {
+              return (
+                  <div>
+                    <Card style={{ width: '18rem' }}>
+                  <Card.Header as="h5">Uploaded At - {new Date(n.createdAt).toTimeString()} </Card.Header>
+                      <Card.Img variant="top" src={n.imageUrl} />
+                      <Card.Body>
+                        <Card.Title>{n.title}</Card.Title>
+                        <Card.Text>
+                          {n.description}
+                        </Card.Text>
+                        <Button variant="primary">View article</Button>
+                      </Card.Body>
+                    </Card>
+                    {/* <Card>
+                    <Card.Header as="h5">{new Date(n.createdAt).toLocaleDateString()} </Card.Header>
+                      <Card.Body>
+                     
+                        <Card.Title>{n.title}</Card.Title>
+                        <Card.Text>
+                          {n.description}
+                        </Card.Text>
+                        <Button variant="primary">View article</Button>
+                      </Card.Body>
+                    </Card> */}
+                  </div>
+              )
+            })}
+        </div>
+
+        <br></br>
+        <hr></hr>
+        <h3>All Articles</h3>
+        <hr></hr>
+        <div className='all_div'>
+
+          {all.map(n => {
+            return (
+              <>
+                <div>
+                  <Card style={{ width: '18rem' }}>
+                  <Card.Header as="h5">{new Date(n.createdAt).toDateString()} At {new Date(n.createdAt).toTimeString()} </Card.Header>
+                    <Card.Img variant="top" src={n.imageUrl} />
+                    <Card.Body>
+                      <Card.Title>{n.title}</Card.Title>
+                      <Card.Text>
+                        {n.description}
+                      </Card.Text>
+                      <Button variant="primary">View article</Button>
+                    </Card.Body>
+                  </Card>
+                </div>
+              </>
+            )
+          })}
+        </div>
+        <div className='footer_div'>
+          <Footer />
+
+        </div>
+      </div >
+    </>
+  );
 }
 
 export default Newscard;
