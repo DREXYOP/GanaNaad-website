8ae16199a5a66292225e8809ac68005ee06d97d7
DREXYOP <91460817+DREXYOP@users.noreply.github.com>
2023-10-20 15:45:58 +0000

    Pending changes exported from your codespace

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ import './App.css';
 import About from './Components/About';
 import Contact from './Components/Contact';
 import Newscard from './Components/News';
+import ArticlePage from "./Components/ArticlePage";
+import Page_404 from "./Components/Page_404";
 
 function App() {
   return (
@@ -19,9 +21,16 @@ function App() {
         <Route path="/about">
           <About />
         </Route>
+        <Route path="/articles/:articleId">
+          <ArticlePage />
+        </Route>
+        <Route path="/news">
+          <Newscard />
+        </Route>
         <Route path="/">
           <Newscard />
         </Route>
+        
       </Switch>
 
 
